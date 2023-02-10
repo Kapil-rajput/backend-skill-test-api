c0d93fe5b6a53c1618bd5eeca94ff50cc292f786
Kapil-rajput <kapil.bholu@gmail.com>
2023-02-10 18:52:13 +0530

    update

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+require("dotenv/config");
 const routes = require("./routes/productsRoutes");
 const app = express();
 const mongoose = require("mongoose");
-require("dotenv/config");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 app.use("/", routes);
 
 mongoose.connect(process.env.DB_URL, () => {
--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,12 +1,26 @@
-module.exports.create = function (req,res) {
-    res.send("Create a post")
-}
-module.exports.display = function (req, res) {
-    res.send("Display all post");
+const Product = require("../models/productsModel");
+
+module.exports.create = async function (req, res) {
+  const product = new Product({
+    name: req.body.name,
+    quantity: req.body.quantity,
+  });
+  try {
+    const saveProduct = await product.save();
+      res.json(saveProduct);
+      console.log(savedProduct)
+  } catch (error) {
+    res.json({
+      message: "Unable To Create Data",
+    });
+  }
 };
-module.exports.delete = function (req, res) {
-    res.send("delete post");
+module.exports.display = async function (req, res) {
+  res.send("Display all post");
 };
-module.exports.update = function (req, res) {
-    res.send("Update post");
+module.exports.delete = async function (req, res) {
+  res.send("delete post");
+};
+module.exports.update = async function (req, res) {
+  res.send("Update post");
 };
