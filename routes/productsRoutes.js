6a6418ae4867ad28b3e5c77190b1e39eb1519484
Kapil-rajput <kapil.bholu@gmail.com>
2023-02-10 20:57:18 +0530

    update

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -4,7 +4,7 @@ const productsController = require('../controllers/productsController')
 router.post("/products/create", productsController.create);
 router.get("/products", productsController.display);
 router.delete("/products/:id", productsController.delete);
-router.post("/products/:id/update_quantity", productsController.create);
+router.post("/products/:id/update_quantity", productsController.update);
 
 
 module.exports = router;
