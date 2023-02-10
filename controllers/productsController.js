180ac3e9cc1e925f155a4ea7489330cdedf6598c
Kapil-rajput <kapil.bholu@gmail.com>
2023-02-10 22:48:28 +0530

    update

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -16,7 +16,7 @@ module.exports.create = async function (req, res) {
 module.exports.display = async function (req, res) {
   try {
     const products = await Product.find();
-      res.json({ products: products });
+    res.json({ products: products });
   } catch (error) {
     console.log(error);
   }
@@ -24,15 +24,21 @@ module.exports.display = async function (req, res) {
 module.exports.delete = async function (req, res) {
   try {
     const productDelete = await Product.deleteOne({ _id: req.params.id });
-      res.send({ message: "Product deleted" });
+    res.send({ message: "Product deleted" });
   } catch (error) {
     console.log(error);
   }
 };
 module.exports.update = async function (req, res) {
-try {
-    const product = await Product.findByIdAndUpdate({_id:req.params.id})
-} catch (error) {
-  
-}
+  try {
+     const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).send({ message: 'Product not found' });
+    }
+    product.quantity += parseInt(req.query.number);
+    const result = await product.save();
+    res.send({ product: result, message: 'Updated successfully' });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
